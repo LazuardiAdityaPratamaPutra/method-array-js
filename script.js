172fcff1731a5e6f296fc0710d6149e93a45ddeb
LazuardiAdityaPratamaPutra <lazuardiaditya17@gmail.com>
2025-08-27 06:24:35 +0700

    second

--- script.js
+++ script.js
@@ -31,4 +31,42 @@ let hasil3 = nama.find(function (e) {
 });
 console.log(hasil3);
 
+// Latihan 4 : Kelola Keranjang Belanja
+let latihan4 = console.log('Latihan 4 : Kelola Keranjang Belanja');
+let buah = ['Apel','Jeruk'];
+// Menambahkan elemen mangga di akhir array
+buah.push('Mangga');
+// Menambahkan elemen pisang di awal array
+buah.unshift('Pisang');
+// Menghilangkan elemen mangga di akhir array
+buah.pop();
+// Menggabungkan elemennya di dalam satu baris
+console.log(buah.join(', '));
+
+// Latihan 5 : Potong dan Sisipkan Data
+let latihan5 = console.log('Latihan 5 : Potong dan Sisipkan Data');
+let angka2 = [10,20,30,40,50];
+// Mengambil elemen 20,30,40
+let hasil4 = angka2.slice(1,3);
+// Menyisipkan angka 25
+hasil4.splice(1,0, 25);
+// Menggabungkan elemennya di dalam satu baris
+console.log(hasil4.join(', '));
+
+// Latihan 6 : Daftar Playlist Musik
+let latihan6 = console.log('Latihan 6 : Daftar Playlist Musik');
+let song = ['Song A','Song B','Song C'];
+let hasil5 = [];
+// Menampilkan semua elemen dengan forEach
+song.forEach(function (e) {
+  hasil5.push(e);
+});
+// Menggabungkan elemennya di dalam satu baris
+console.log(hasil5.join(','));
+
+
+
+
+
+
 
